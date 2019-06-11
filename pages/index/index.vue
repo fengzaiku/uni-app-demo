7b282d8238153def5e55b3e4e3bff91f90aae73e
lxs <xunaiya@sina.com>
2019-06-12 00:54:33 +0800

    代码更新

---
+++ pages/index/index.vue
@@ -0,0 +1,95 @@
+<template>
+    <view>
+        <view class="uni-padding-wrap uni-common-mt">
+            <view>
+				<video preload="metadata" controls src="http://video.699pic.com/videos/89/75/71/a_UIBj3pHQYXcV1555897571.mp4" type="video/mp4"></video>
+                <video id="myVideo" src="https://video.699pic.com/videos/89/75/71/a_UIBj3pHQYXcV1555897571.mp4"
+                    @error="videoErrorCallback" :danmu-list="danmuList" enable-danmu danmu-btn controls></video>
+            </view>
+            <!-- #ifndef MP-ALIPAY -->
+            <view class="uni-list uni-common-mt">
+                <view class="uni-list-cell">
+                    <view>
+                        <view class="uni-label">弹幕内容</view>
+                    </view>
+                    <view class="uni-list-cell-db">
+                        <input v-model="danmuValue" class="uni-input" type="text" placeholder="在此处输入弹幕内容" />
+                    </view>
+                </view>
+            </view>
+            <view class="uni-btn-v">
+                <button @click="sendDanmu" class="page-body-button">发送弹幕</button>
+            </view>
+            <!-- #endif -->
+        </view>
+    </view>
+</template>
+
+<script>
+	export default {
+    data() {
+        return {
+            src: '',
+            danmuList: [{
+                    text: '第 1s 出现的弹幕',
+                    color: '#ff0000',
+                    time: 1
+                },
+                {
+                    text: '第 3s 出现的弹幕',
+                    color: '#ff00ff',
+                    time: 3
+                }
+            ],
+            danmuValue: ''
+        }
+    },
+    onReady: function(res) {
+        // #ifndef MP-ALIPAY
+        this.videoContext = uni.createVideoContext('myVideo')
+        // #endif
+    },
+    methods: {
+        sendDanmu: function() {
+            this.videoContext.sendDanmu({
+                text: this.danmuValue,
+                color: this.getRandomColor()
+            });
+            this.danmuValue = '';
+        },
+        videoErrorCallback: function(e) {
+            uni.showModal({
+                content: e.target.errMsg,
+                showCancel: false
+            })
+        },
+        getRandomColor: function() {
+            const rgb = []
+            for (let i = 0; i < 3; ++i) {
+                let color = Math.floor(Math.random() * 256).toString(16)
+                color = color.length == 1 ? '0' + color : color
+                rgb.push(color)
+            }
+            return '#' + rgb.join('')
+        }
+    }
+}
+</script>
+
+<style>
+	.content {
+		text-align: center;
+		height: 400upx;
+	}
+
+	.logo {
+		height: 200upx;
+		width: 200upx;
+		margin-top: 200upx;
+	}
+
+	.title {
+		font-size: 36upx;
+		color: #8f8f94;
+	}
+</style>
